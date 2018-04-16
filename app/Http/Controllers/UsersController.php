42d70b8c9ef91734a51f5501cced0c8da03bad62
易亮亮 <1095036273@qq.com>
2018-04-16 10:09:41 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,18 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+    }
+
+
     public function create()
     {
         return view('users.create');
     }
 
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
+
 
     public function store(Request $request)
     {
@@ -36,5 +47,32 @@ class UsersController extends Controller
         Auth::login($user);
 
         return redirect()->route('users.show', [$user]);
+    }
+
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
     }
 }
